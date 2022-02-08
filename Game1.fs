f91bb3bdbff7ab3f1e201cc373e5396859365db3
Dan Liebgold <dliebgold@wonderstorm.net>
2022-02-07 16:18:22 -0800

    working example; add initial content assets

--- Game1.fs
+++ Game1.fs
@@ -3,12 +3,14 @@ namespace fs
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
+open Microsoft.Xna.Framework.Content
 
 type Game1 () as this =
     inherit Game()
 
     let graphics = new GraphicsDeviceManager(this)
     let mutable spriteBatch = Unchecked.defaultof<_>
+    let mutable Tex : Texture2D option = None
 
     do
         this.Content.RootDirectory <- "Content"
@@ -21,8 +23,9 @@ type Game1 () as this =
 
     override this.LoadContent() =
         spriteBatch <- new SpriteBatch(this.GraphicsDevice)
-
+        
         // TODO: use this.Content to load your game content here
+        Tex <- Some (this.Content.Load("Basic1"))
 
     override this.Update (gameTime) =
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -36,6 +39,9 @@ type Game1 () as this =
         this.GraphicsDevice.Clear Color.CornflowerBlue
 
         // TODO: Add your drawing code here
-
         base.Draw(gameTime)
+
+        spriteBatch.Begin()
+        spriteBatch.Draw(Tex.Value, Vector2(10.f,60.f), Color.White)
+        spriteBatch.End()
 
