9858a7e56cf8101125e83ef66728229621c36385
Dan Liebgold <dan.liebgold@gmail.com>
2022-02-14 08:26:23 -0800

    fix build_content paths

--- build_content.ps1
+++ build_content.ps1
@@ -1,12 +1,14 @@
 #Requires -Version 6.0
 $ErrorActionPreference = "Stop"
 
+## dotnet /Users/dliebgold/.nuget/packages/monogame.content.builder.task/3.8.0.1641/tools/netcoreapp3.1/any/mgcb.dll `
 ##    /f:"../../../../.nuget/packages/monogame.extended.content.pipeline/3.8.0/tools/MonoGame.Extended.Content.Pipeline.dll" `
+## /f:"/Users/dliebgold/proj/misc/MonoGame.Extended/src/cs/MonoGame.Extended.Content.Pipeline/bin/netcoreapp3.1/publish/MonoGame.Extended.Content.Pipeline.dll" `
 
-dotnet /Users/dliebgold/.nuget/packages/monogame.content.builder.task/3.8.0.1641/tools/netcoreapp3.1/any/mgcb.dll `
-    /f:"/Users/dliebgold/proj/misc/MonoGame.Extended/src/cs/MonoGame.Extended.Content.Pipeline/bin/netcoreapp3.1/publish/MonoGame.Extended.Content.Pipeline.dll" `
-    /@:"/Users/dliebgold/proj/misc/ashers_game/Content/Content.mgcb" `
+dotnet tool run mgcb `
+    /f:"../../../../.nuget/packages/monogame.extended.content.pipeline/3.8.0/tools/MonoGame.Extended.Content.Pipeline.dll" `
+    /@:"Content/Content.mgcb" `
     /platform:DesktopGL `
-    /outputDir:"/Users/dliebgold/proj/misc/ashers_game/Content/bin/DesktopGL/Content" `
-    /intermediateDir:"/Users/dliebgold/proj/misc/ashers_game/Content/obj/DesktopGL/Content" `
-    /workingDir:"/Users/dliebgold/proj/misc/ashers_game/Content/"
+    /outputDir:"Content/bin/DesktopGL/Content" `
+    /intermediateDir:"Content/obj/DesktopGL/Content" `
+    /workingDir:"Content"
